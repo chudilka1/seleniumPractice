ac2104a06bef1bc3d5f7b9b729d230ef6e71417f
Aleksandr Yepishev <aleksandr.yepishev@uvoteam.com>
2017-04-04 14:37:54 +0300

    Change WebDriverTestBase (added .properties)

---
+++ src/main/java/com/chudilka1/core/BrowsersName.java
@@ -0,0 +1,6 @@
+package com.chudilka1.core;
+
+public enum BrowsersName {
+
+    CHROME, FIREFOX
+}
--- src/main/java/com/chudilka1/core/WebDriverTestBase.java
+++ src/main/java/com/chudilka1/core/WebDriverTestBase.java
@@ -1,34 +1,112 @@
 package com.chudilka1.core;
 
+import com.chudilka1.util.PropertiesCache;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public abstract class WebDriverTestBase {
+    private static final String WEB_DRIVER_GECKO = "webdriver.gecko";
+    private static final String WEB_DRIVER_CHROME = "webdriver.chrome";
+    private static final String CHROME_PATH_WIN = "chrome.path.win";
+    private static final String CHROME_PATH_UNIX = "chrome.path.unix";
+    private static final String GECKO_DRIVER_PATH_WIN = "geckodriver.path.win";
+    private static final String GECKO_DRIVER_PATH_UNIX = "geckodriver.path.unix";
+    private static final String IMPLICIT_WAIT = "webdriver.implicit.wait";
+    private static final String SCRIPT_TIMEOUT = "webdriver.script.timeout";
+    private static final String LOAD_TIMEOUT = "webdriver.load.timeout";
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+    private static final String BROWSER = System.getProperty("browser");
+    //private static final Logger LOG = LogManager.getLogger(MethodHandles.lookup().lookupClass());
     protected WebDriver driver;
+    private List unixOSCodes = Arrays.asList("nix", "nux", "aix");
+    private List windowsOSCodes = Arrays.asList("win");
+    private DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
-    @BeforeClass
+    @BeforeSuite
     public void setUp() {
-        //System.setProperty("webdriver.gecko.driver", WebDriverTestBase.class.getClassLoader().getResource("geckodriver.exe").getPath());
-        //System.setProperty("webdriver.chrome.driver", WebDriverTestBase.class.getClassLoader().getResource("chromedriver.exe").getPath());
-        System.setProperty("webdriver.chrome.driver", WebDriverTestBase.class.getClassLoader().getResource("chromedriver").getPath());
-        //driver = new FirefoxDriver();
-        driver = new ChromeDriver();
+        if (isWindows()) {
+            if (isBrowserSetUpFor(BrowsersName.CHROME.name(), BROWSER)) {
+                System.setProperty(getProperty(WEB_DRIVER_CHROME), getPath(getProperty(CHROME_PATH_WIN)));
+            } else if (isBrowserSetUpFor(BrowsersName.FIREFOX.name(), BROWSER)) {
+                System.setProperty(getProperty(WEB_DRIVER_GECKO), getPath(getProperty(GECKO_DRIVER_PATH_WIN)));
+            }
+        } else if (isUnix()) {
+            if (isBrowserSetUpFor(BrowsersName.CHROME.name(), BROWSER)) {
+                System.setProperty(getProperty(WEB_DRIVER_CHROME), getPath(getProperty(CHROME_PATH_UNIX)));
+            } else if (isBrowserSetUpFor(BrowsersName.FIREFOX.name(), BROWSER)) {
+                System.setProperty(getProperty(WEB_DRIVER_GECKO), getPath(getProperty(GECKO_DRIVER_PATH_UNIX)));
+            }
+        }
+        /*if (isBrowserSetUpFor(BrowsersName.CHROME.name(), BROWSER)) {
+            System.setProperty(getProperty(WEB_DRIVER_CHROME), getPath(getProperty(CHROME_PATH_WIN)));
+        } else if (isBrowserSetUpFor(BrowsersName.FIREFOX.name(), BROWSER)) {
+            System.setProperty(getProperty(WEB_DRIVER_GECKO), getPath(getProperty(GECKO_DRIVER_PATH_WIN)));
+            if (isWindows()) {
+                System.setProperty(getProperty(WEB_DRIVER_GECKO), getPath(getProperty(GECKO_DRIVER_PATH_WIN)));
+            } else if (isUnix()) {
+                System.setProperty(getProperty(WEB_DRIVER_GECKO), getPath(getProperty(GECKO_DRIVER_PATH_UNIX)));
+            }
+        }*/
+        initializeWebDriver();
+    }
+
+    private void initializeWebDriver() {
+        if (isBrowserSetUpFor(BrowsersName.CHROME.name(), BROWSER)) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-extensions");
+            driver = new ChromeDriver();
+            desiredCapabilities.setBrowserName(BrowsersName.CHROME.name());
+        } else if (isBrowserSetUpFor(BrowsersName.FIREFOX.name(), BROWSER)) {
+            driver = new FirefoxDriver();
+            desiredCapabilities.setBrowserName(BrowsersName.FIREFOX.name());
+        }
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(40, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(Integer.valueOf(getProperty(SCRIPT_TIMEOUT)), TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(Integer.valueOf(getProperty(LOAD_TIMEOUT)), TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Integer.valueOf(getProperty(IMPLICIT_WAIT)), TimeUnit.SECONDS);
     }
 
     @AfterClass
     public void tearDown() {
-        //driver.manage().deleteAllCookies();
-        //driver.close(); //closes tab
-        //driver.quit(); //closes browser
+        //driver.quit();
     }
 
+    private boolean isBrowserSetUpFor(String browserName, String browserSystemVeriable) {
+        return StringUtils.isEmpty(BROWSER) || browserName.equalsIgnoreCase(browserSystemVeriable);
+    }
+
+    private boolean isWindows() {
+        return OS.contains("win");
+    }
+
+    private boolean isUnix() {
+        return OS.contains("nix") || OS.contains("nux") || OS.contains("aix");
+    }
+
+    private String getProperty(String key) {
+        return PropertiesCache.getInstance().getProperty(key);
+    }
+
+    private String getPath(String s) {
+        String path = null;
+        URL url = WebDriverTestBase.class.getClassLoader().getResource(s);
+        if (url != null) {
+            path = url.getPath();
+        } else {
+            //LOG.error("Not found resource");
+            System.out.println("Not found resource");
+        }
+        return path;
+    }
 }
